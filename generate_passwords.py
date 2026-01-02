16a00239e303a727321791a09c3848cf652e1f7d
saymonbtw <coolmansemen@gmail.com>
2026-01-02 14:54:03 +0300

    генератор паролей

--- generate_passwords.py
+++ generate_passwords.py
@@ -1,7 +1,9 @@
 from random import *
 
+
 def generate_password(lenght, symbols):
     return "".join([choice(symbols) for _ in range(lenght)])
+
 
 digits = "0123456789"
 lowcase_tetters = "abcdefghijklmnopqrstuvwxyz"
@@ -18,13 +20,18 @@ need_lowcase = input("Включать ли строчные буквы? (y/n) "
 need_punc = input("Включать ли символы? (y/n) ")
 del_amb_symbols = input("Исключать ли неоднозначные символы il1Lo0O? (y/n) ")
 
-if need_digit == "y": chars += digits
-if need_lowcase == "y": chars += lowcase_tetters
-if need_upcase == "y": chars += uppercase_letters
-if need_punc == "y": chars += punctuation
-if del_amb_symbols == "y": chars
+if need_digit == "y":
+    chars += digits
+if need_lowcase == "y":
+    chars += lowcase_tetters
+if need_upcase == "y":
+    chars += uppercase_letters
+if need_punc == "y":
+    chars += punctuation
 if del_amb_symbols == "y":
-    chars = ''.join([c for c in chars if c not in ambiguous])
+    chars
+if del_amb_symbols == "y":
+    chars = "".join([c for c in chars if c not in ambiguous])
 
 for _ in range(cnt_passwords):
     print(generate_password(len_password, chars))
